5976a409fce55d534077586ebd260f47fd04df9b
UtkarshKunwar <utkarsh2602@gmail.com>
2019-01-27 23:56:19 +0530

    Added GLava env for KWin.
    Otherwise GLava shows up in the task switcher.

---
+++ glava/env_KWin.glsl
@@ -0,0 +1,8 @@
+#request setdecorated false
+#request setxwintype "utility"
+#request addxwinstate "below"
+#request addxwinstate "skip_taskbar"
+#request addxwinstate "skip_pager"
+#request addxwinstate "pinned"
+#request setclickthrough true
+
